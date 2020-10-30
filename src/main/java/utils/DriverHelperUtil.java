8955be191eb28926af6052b85c9bc2486f99c522
cpanzaru <claudiudamian.panzaru@endava.com>
2020-10-30 17:06:40 +0200

    Add parallel running

--- src/main/java/utils/DriverHelperUtil.java
+++ src/main/java/utils/DriverHelperUtil.java
@@ -2,6 +2,8 @@ package utils;
 
 import lombok.extern.log4j.Log4j2;
 
+import static utils.InitializationUtil.getDriver;
+
 @Log4j2
 public class DriverHelperUtil {
     public static void waitForPage() {
@@ -10,7 +12,7 @@ public class DriverHelperUtil {
         String previous = "";
         String current;
         while (stillChanging) {
-            current = InitializationUtil.getDriver().getPageSource();
+            current = getDriver().getPageSource();
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
